9c42da2dcec883175cda47eada378a3088dcc00a
༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻⁩ <152390226+Bhadbanks@users.noreply.github.com>
2025-10-31 14:24:04 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,59 @@
+
+// 漏 2025 Debraj. All Rights Reserved.
+// respect the work, don鈥檛 just copy-paste.
+
+const fs = require('fs')
+
+const config = {
+    owner: "-",
+    botNumber: "-",
+    setPair: "K0MRAID1",
+    thumbUrl: "https://files.catbox.moe/alasef.jpeg",
+    session: "sessions",
+    status: {
+        public: true,
+        terminal: true,
+        reactsw: false
+    },
+    message: {
+        owner: "no, this is for owners only",
+        group: "this is for groups only",
+        admin: "this command is for admin only",
+        private: "this is specifically for private chat"
+    },
+    mess: {
+        owner: 'This command is only for the bot owner!',
+        done: 'Mode changed successfully!',
+        error: 'Something went wrong!',
+        wait: 'Please wait...'
+    },
+    settings: {
+        title: "simple and light weight",
+        packname: '༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻',
+        description: "created with 🤍 by ༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻ᵀʰᵉ ᵁˡᵗᶦᵐᵃᵗᵘᵐ",
+        author: 'https://www.github.com/',
+        footer: "༺𝕿𝖍𝖊 𝕴𝖑𝖙𝖎𝖒𝖆𝖙𝖚𝖒༻"
+    },
+    newsletter: {
+        name: "created with 🤍 by ༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻ᵀʰᵉ ᵁˡᵗᶦᵐᵃᵗᵘᵐ",
+        id: "0@newsletter"
+    },
+    api: {
+        baseurl: "https://hector-api.vercel.app/",
+        apikey: "hector"
+    },
+    sticker: {
+        packname: "༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻",
+        author: "༺𝕷𝖔𝖜𝖐𝖊𝖞 𝕴𝖘 𝕳𝖎𝖒༻"
+    }
+}
+
+module.exports = config;
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
